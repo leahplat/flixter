646e0f23df061dba3d02a3427086c2a89a80b1dd
Leah Plat <leahplat@gmail.com>
2019-06-17 13:56:28 -0400

    lesson 21

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -34,4 +34,4 @@ class Instructor::CoursesController < ApplicationController
   def course_params
     params.require(:course).permit(:title, :description, :cost)
   end
-end 
+end
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,11 +2,10 @@ class LessonsController < ApplicationController
   def show
   end
 
-    private
+  private
 
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
-end 
 end 
