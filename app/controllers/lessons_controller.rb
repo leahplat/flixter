037ea9423b96384859fddb155f4122a4a9dd84dc
Leah Plat <leahplat@gmail.com>
2019-06-20 17:00:31 -0400

    lesson 28

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,28 @@
 class LessonsController < ApplicationController
-  def show
-  end
-
-  private
-
-  helper_method :current_lesson
-  def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
-  end
-end 
+    before_action :authenticate_user!
+    before_action :require_enrolled_for_current_course
+    
+    def show
+    end
+    
+    
+    private
+    
+    def require_enrolled_for_current_course
+       if ! current_user.enrolled_in?(current_course)
+           redirect_to course_path(current_lesson.section.course), alert: 'Whoops! You aren\'t enrolled in this course yet!'
+       else
+           
+       end
+    end
+    
+    helper_method :current_lesson
+    def current_lesson
+       @current_lesson ||= Lesson.find(params[:id]) 
+    end
+    
+    helper_method :current_course
+    def current_course
+        current_lesson.section.course
+    end
+end
