a7b3c3a1d37f1dba0cea072fbc2fe425982ea8ab
Leah Plat <leahplat@gmail.com>
2019-07-05 15:39:04 -0400

    lesson adding

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,33 +1,21 @@
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_section, only: [:create]
-  before_action :require_authorized_for_current_lesson, only: [:update]
+  before_action :require_authorized_for_current_section
+
+  def new
+    @lesson = Lesson.new
+  end
 
   def create
     @lesson = current_section.lessons.create(lesson_params)
     redirect_to instructor_course_path(current_section.course)
   end
 
-  def update
-    current_lesson.update_attributes(lesson_params)
-    render plain: 'updated!'
-  end
-
   private
-
-  def require_authorized_for_current_lesson
-    if current_lesson.section.course.user != current_user
-      render plain: 'Unauthorized', status: :unauthorized
-    end
-  end
-
-  def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
-  end
 
   def require_authorized_for_current_section
     if current_section.course.user != current_user
-      render plain: 'Unauthorized', status: :unauthorized
+      return render plain: 'Unauthorized', status: :unauthorized
     end
   end
 
@@ -37,6 +25,6 @@ class Instructor::LessonsController < ApplicationController
   end
 
   def lesson_params
-    params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
+    params.require(:lesson).permit(:title, :subtitle, :video)
   end
 end 
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,29 +1,32 @@
 class LessonsController < ApplicationController
-    before_action :authenticate_user!
-    # before_action :require_enrolled_for_current_course
-    before_action :require_authorized_for_lessons, only: [:show]
-    
-    def show
+  before_action :authenticate_user!
+  before_action :require_authorized_for_lessons, only: [:show]
+
+  def show
+  end
+
+  private
+
+  def require_authorized_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      return render plain: 'Unauthorized', status: :unauthorized
     end
-    
-    
-    private
-    
-    def require_enrolled_for_current_course
-       if ! current_user.enrolled_in?(current_course)
-           redirect_to course_path(current_lesson.section.course), alert: 'Whoops! You aren\'t enrolled in this course yet!'
-       else
-           
-       end
+  end
+
+  def require_authorized_for_lessons
+    if current_user.enrolled_in?(current_lesson.section.course) != true
+      redirect_to course_path(current_lesson.section.course), alert: 'Whoops! You\'re not enrolled in this class yet!'
     end
-    
-    helper_method :current_lesson
-    def current_lesson
-       @current_lesson ||= Lesson.find(params[:id]) 
-    end
-    
-    helper_method :current_course
-    def current_course
-        current_lesson.section.course
-    end
+  end
+
+  helper_method :current_lesson
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end
 end
+
+ def require_authorized_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      return render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
