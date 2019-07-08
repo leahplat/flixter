ebfef3cc22f979252a3e56b799130bd65ae32c45
Leah Plat <leahplat@gmail.com>
2019-07-08 09:21:48 -0400

    mixed content fix

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -25,8 +25,4 @@ class LessonsController < ApplicationController
   end
 end
 
- # def require_authorized_for_current_lesson
- #    if current_lesson.section.course.user != current_user
- #      return render plain: 'Unauthorized', status: :unauthorized
- #    end
- #  end
+ 
